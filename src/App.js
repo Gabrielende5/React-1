e7eb9d427384ef1d936f67146131b07bcaeb25bb
Gabrielende5 <gabrielende53@gmail.com>
2024-02-22 17:22:55 -0300

    UseEffect + UseState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  // const (variavel, functionParaMudarValorDaVariavel) = useState("valor inicial");
+  // "UseEffect" = muda o valor da variavel no html (renderiza na tela); "UseState" = muda o valor da variavel no codigo
+  const [useEffectCounter,SetUseEffectCounter] = useState(0);
+  const [logado,setlogado]=useState(false);
+  useEffect(()=>{
+    console.log("useEffect chamado");
+    SetUseEffectCounter(useEffectCounter + 1)
+  }, [logado])
+  const Logar = () => {
+    setlogado(true)
+  }
+  const Deslogar = () => {
+    setlogado(false)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1> Use effect foi chamado {useEffectCounter} vezes </h1>
+      {logado ? <p>Logado</p> : <p> Deslogado</p>} 
+      <button onClick={Logar}>Logar</button>
+      <button onClick={Deslogar}>Deslogar</button>
+      
     </div>
   );
+  // "?" = if / ":" = else 
 }
 
 export default App;
